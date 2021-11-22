a5b9f90e9cfe820ff63689289d11bd18542c2c60
DHANUSH H V <67680433+DHANUSH-web@users.noreply.github.com>
2021-11-22 13:42:50 +0530

    Update b2d_d2b.cpp

--- BinDec/b2d_d2b.cpp
+++ BinDec/b2d_d2b.cpp
@@ -1,5 +1,6 @@
 // DHANUSH H V  https://www.github.com/DHANUSH-web
 // Both binary to decimal and decimal to binary in a single program
+// Programmed in: C++
 
 #include <iostream>
 #include <cstring>
